6fdc000e7cf2ba9a440677a6ee7aeb583c14290a
tom <odinzero@outlook.com>
2023-01-06 23:08:11 +0800

    在 App.ts 导入全局样式并测试效果

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. css 库(styled-components)
    2.1 npm i styled-components & npm i -D @types/styled-components
    2.2 reset-css 使用 https://www.joshwcomeau.com/css/custom-css-reset/ src/style.ts
+   2.3 在 App.ts 导入全局样式并测试效果
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { IconStyle } from "./assets/iconfont";
+import { GlobalStyle } from "./style";
+
 function App() {
-  return <div className="App"> Learn React</div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <IconStyle />
+      <i className="iconfont">&#xe62b;</i>
+    </div>
+  );
 }
 
 export default App;
