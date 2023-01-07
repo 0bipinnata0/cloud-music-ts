f1f3e5656d006c3e8dac3b1d951ab909c091bee6
tom <odinzero@outlook.com>
2023-01-07 12:01:41 +0800

    歌手详情mock

--- src/application/Singers/index.tsx
+++ src/application/Singers/index.tsx
@@ -1,33 +1,73 @@
 import React, { useState } from "react";
 import { alphaTypes, categoryTypes } from "../../api/config";
 import Horizon from "../../baseUI/horizon";
-import { NavContainer } from "./style";
-const Singers: React.FC<{}> = (props) => {
-  let [category, setCategory] = useState("");
-  let [alpha, setAlpha] = useState("");
+import Scroll from "../../baseUI/scroll";
+import { NavContainer, List, ListItem, ListContainer } from "./style";
+//mock 数据
+const singerList = new Array(33).fill(0).map((item) => {
+  return {
+    picUrl:
+      "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+    name: "隔壁老樊",
+    accountId: 277313426,
+  };
+});
 
-  let handleUpdateAlpha = (val: string) => {
+// 渲染函数，返回歌手列表
+const SingerList = () => {
+  return (
+    <List>
+      {singerList.map((item, index) => {
+        return (
+          <ListItem key={item.accountId + "" + index}>
+            <div className="img_wrapper">
+              <img
+                src={`${item.picUrl}?param=300x300`}
+                width="100%"
+                height="100%"
+                alt="music"
+              />
+            </div>
+            <span className="name">{item.name}</span>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+const Singers: React.FC<{}> = (props) => {
+  const [category, setCategory] = useState("");
+  const [alpha, setAlpha] = useState("");
+
+  const handleUpdateAlpha = (val: string) => {
     setAlpha(val);
   };
 
-  let handleUpdateCategory = (val: string) => {
+  const handleUpdateCategory = (val: string) => {
     setCategory(val);
   };
   return (
-    <NavContainer>
-      <Horizon
-        list={categoryTypes}
-        title={"分类 (默认热门):"}
-        handleClick={handleUpdateCategory}
-        oldVal={category}
-      />
-      <Horizon
-        list={alphaTypes}
-        title={"首字母:"}
-        handleClick={(val) => handleUpdateAlpha(val)}
-        oldVal={alpha}
-      />
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <Horizon
+          list={categoryTypes}
+          title={"分类 (默认热门):"}
+          handleClick={handleUpdateCategory}
+          oldVal={category}
+        />
+        <Horizon
+          list={alphaTypes}
+          title={"首字母:"}
+          handleClick={(val) => handleUpdateAlpha(val)}
+          oldVal={alpha}
+        />
+      </NavContainer>
+      <ListContainer>
+        <Scroll>
+          <SingerList />
+        </Scroll>
+      </ListContainer>
+    </div>
   );
 };
 
